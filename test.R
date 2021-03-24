40de9dc88aa831431785ea08380eded696ce7ea0
ykunisato <ykunisato@gmail.com>
2021-03-24 08:06:37 +0000

    ちょっとはじめたけど，まだ良くわからない

--- test.R
+++ test.R
@@ -1,1 +1,14 @@
 library(osfr)
+
+# osf_auth("ThIsIsNoTaReAlPaTbUtYoUgEtIt")
+
+eln4osfr_node <- osf_retrieve_node("https://osf.io/tf2he")
+labnote <- osf_create_component(eln4osfr_node, title = "labnote")
+labnote_fig <- osf_create_component(labnote, title = "fig")
+osf_upload(labnote, path = "2021_03_24.md")
+
+osf_upload(labnote, path = "demo.png")
+osf_upload(labnote, path = "2021_03_22.md")
+
+#むむむ。osfにラボノートセクションを作って，そこにmdファイルを追加していく戦略だが，
+#画像の連携がまずいな・・・
