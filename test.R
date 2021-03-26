07bd69ab0227297ffdca2ee601d9c7de6364ccfd
Yoshihiko Kunisato <ykunisato@psy.senshu-u.ac.jp>
2021-03-26 07:19:23 +0000

    まだ模索中

--- test.R
+++ test.R
@@ -4,11 +4,20 @@ library(osfr)
 
 eln4osfr_node <- osf_retrieve_node("https://osf.io/tf2he")
 labnote <- osf_create_component(eln4osfr_node, title = "labnote")
-labnote_fig <- osf_create_component(labnote, title = "fig")
-osf_upload(labnote, path = "2021_03_24.md")
 
+#　labnoteが複数できてしまった。コンポーネントにアクセスする方法が必要
+
+# 画像を別にアップする
 osf_upload(labnote, path = "demo.png")
-osf_upload(labnote, path = "2021_03_22.md")
+
+# Markdownでどう挿入するかが悩ましい
+osf_upload(labnote, path = "2021_03_25.md")
+
+# 画像ファイルをアップロードしたら，そのファイルのリンクアドレスを返す関数がほしい。
+# それをmarkdownに貼り付けをすればいい。
+
+# 上書きする場合
+osf_upload(labnote, path = "2021_03_22.md",conflicts = "overwrite")
 
 #むむむ。osfにラボノートセクションを作って，そこにmdファイルを追加していく戦略だが，
 #画像の連携がまずいな・・・
